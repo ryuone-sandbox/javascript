b5e9e6057e4b2d977b2afc7b8fe6e9ba5ee6c560
Ryuichi Maeno <ryuone@gmail.com>
2017-11-30 22:33:38 +0900

    Add study/step01.

---
+++ study/step01/app.js
@@ -0,0 +1,15 @@
+$(function() {
+  const inputElem = $('#newTodo');
+  const ulElem = $('#todoList');
+
+  $('button#addBtn').click(function(e) {
+    const val = inputElem.val();
+    inputElem.val('');
+    ulElem.append($('<li>' + val + '</li>'));
+  });
+
+  $('ul#todoList').click(function(e){
+    $(e.target).remove();
+  })
+
+});
